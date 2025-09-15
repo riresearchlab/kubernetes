6e34e31dd72a908bbe4991e049d29f1ae6c861aa
Md. Rakib Trofder <mdrakibtrofer@gmail.com>
2025-09-15 21:35:55 +0600

    refactor: remove unused FeatureHighlights component
    The component was removed from the Index page and deleted entirely as it was no longer being used in the application. This cleanup helps maintain a cleaner codebase by removing unused code.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,4 @@
 import { Hero } from "@/components/Hero";
-import { FeatureHighlights } from "@/components/FeatureHighlights";
 import { KubernetesFundamentals } from "@/components/KubernetesFundamentals";
 import { StructuredLearningPath } from "@/components/StructuredLearningPath";
 import { Footer } from "@/components/Footer";
@@ -8,7 +7,6 @@ const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <Hero />
-      <FeatureHighlights />
       <KubernetesFundamentals />
       <StructuredLearningPath />
       <Footer />
